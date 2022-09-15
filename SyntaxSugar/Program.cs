6ac60abc7a3f2eb89e9219b697da948ce3877600
Braydon Sutherland <BraydonSutherland@gmail.com>
2022-09-15 11:06:38 -0500

    syntax sugar

--- SyntaxSugar/Program.cs
+++ SyntaxSugar/Program.cs
@@ -1,3 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SyntaxSugar;
+using static System.Console;
 
+
+Run.Withdrawl(600);
---
+++ SyntaxSugar/Run.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+namespace SyntaxSugar
+{
+    public class Run
+    {
+        public static void Withdrawl(double num1)
+        {
+            double balance = 500.00;
+            var results = (balance - num1 < 0) ? $"You don't have enough funds. Total Funds: {balance}" : $"Withdrawl Complete, Balance Remaining: {balance - num1}";
+            WriteLine(results);
+            ReadKey();
+        }
+    }
+}
